2efb66e8db6a6864cec07e470a3013d4256aa27f
Aleksandr-Zhazhoyan <zhazhoyan85@gmail.com>
2025-04-04 20:07:33 +0400

    Initial Commit

---
+++ To_Do_List25/To_Do_List25App.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_List25App.swift
+//  To_Do_List25
+//
+//  Created by Aleksandr Zhazhoyan on 04.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_List25App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
